bd9e5e6fd7be329019a1cb0c477e9976f58e3d6e
Sigenot <tobias.kreil@proton.me>
2025-07-27 10:49:06 +0200

    adjusted model due to layer issues

--- model_arch.py
+++ model_arch.py
@@ -5,32 +5,33 @@ class EmotionCNN(nn.Module):
     def __init__(self):
         super(EmotionCNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, padding=2),
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, padding=2),
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
         self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, padding=2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
         self.layer4 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, padding=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
-        self.fc = nn.Linear(7 * 7 * 128, 6) # 6 emotions
+        self.fc = nn.Linear(4 * 4 * 128, 6) # 6 emotions
     
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = out.view(out.size(0), -1)
+        #print(out.shape)
+        out = out.view(out.size(0), -1) #flatten tensors
         out = self.fc(out)
         return out
