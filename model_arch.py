d38101c93c8f2ef86ce414fd9b564607c7288fc5
Sigenot <tobias.kreil@proton.me>
2025-07-27 16:28:53 +0200

    experimental tweak for better results

--- model_arch.py
+++ model_arch.py
@@ -5,26 +5,39 @@ class EmotionCNN(nn.Module):
     def __init__(self):
         super(EmotionCNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25)
         )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25)
         )
         self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25)
         )
         self.layer4 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25)
         )
-        self.fc = nn.Linear(4 * 4 * 128, 6) # 6 emotions
+        self.fc1 = nn.Sequential(
+            nn.Linear(4 * 4 * 256, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5)
+        )
+        self.fc2 = nn.Linear(512, 6) # 6 emotions
     
     def forward(self, x):
         out = self.layer1(x)
@@ -33,5 +46,6 @@ class EmotionCNN(nn.Module):
         out = self.layer4(out)
         #print(out.shape)
         out = out.view(out.size(0), -1) #flatten tensors
-        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
         return out
