9067b2fd32d4d2fe3fbb58bcd04b48ebb7391cea
Sigenot <tobias.kreil@proton.me>
2025-07-27 10:47:08 +0200

    issues regarding channels, permuted order

--- dataloader.py
+++ dataloader.py
@@ -8,7 +8,7 @@ train_faces, train_labels = load_img_and_lbl('train')
 test_faces, test_labels = load_img_and_lbl('test')
 
 # convert to pytorch-tensors
-faces_tensor = torch.tensor(train_faces, dtype=torch.float32)
+faces_tensor = torch.tensor(train_faces, dtype=torch.float32).permute(0, 3, 1, 2)
 labels_tensor = torch.tensor(np.argmax(train_labels, axis=1), dtype=torch.long)
 
 # custom dataset
@@ -28,7 +28,7 @@ train_dataset = CustomDataset(faces_tensor, labels_tensor)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 
 # repeat for test data
-test_faces_tensor = torch.tensor(test_faces, dtype=torch.float32)
+test_faces_tensor = torch.tensor(test_faces, dtype=torch.float32).permute(0, 3, 1, 2)
 test_labels_tensor = torch.tensor(np.argmax(test_labels, axis=1), dtype=torch.long)
 test_dataset = CustomDataset(test_faces_tensor, test_labels_tensor)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
