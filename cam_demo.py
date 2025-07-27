3f2978613d634e7143cd6cf6b9a84282f0d6e193
Sigenot <tobias.kreil@proton.me>
2025-07-27 20:40:25 +0200

    refine camera demo, still wip

--- cam_demo.py
+++ cam_demo.py
@@ -1,8 +1,14 @@
 import cv2
 import numpy as np
 import torch
+import torch.nn.functional as F
+import time
+import warnings
 from model_arch import EmotionCNN
-import preprocess
+from preprocess import preprocess_img
+
+# suppress compatbility warnings
+warnings.filterwarnings("ignore")
 
 # load pretrained model
 model = EmotionCNN()
@@ -28,19 +34,33 @@ while True:
 
     for (x, y, w, h) in faces:
         face_img = frame[y:y+h, x:x+w]
-        processed_face = preprocess(face_img)
+        processed_face = preprocess_img(face_img)
 
         # predict emotions
         with torch.no_grad():
             emotion_prediction = model(processed_face)
-        emotion_label = emotion_labels[torch.argmax(emotion_prediction)]
+            probabilities = F.softmax(emotion_prediction, dim=1)[0] * 100
+
+        #emotion_label = emotion_labels[torch.argmax(emotion_prediction)]
 
         # draw a rectangle around face and label predicted emotion
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        cv2.putText(frame, emotion_label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        #cv2.putText(frame, emotion_label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+        # show probability of all emotions
+        text_x = x + w + 100
+        y_offset = 20
+
+        # show probability of all emotions
+        for i, (label, prob) in enumerate(zip(emotion_labels, probabilities)):
+            text = f"{label}: {prob:.2f}%"
+            cv2.putText(frame, text, (x, y + y_offset * i -10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
     # show frame
     cv2.imshow('Emotion Recognition', frame)
+
+    # reduce refresh rate
+    time.sleep(0.1)
 
     # end the loop when q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
