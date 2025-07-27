5e0195f73de9a1d2a1813d35dde941f0d7ccde6d
Sigenot <tobias.kreil@proton.me>
2025-07-27 16:29:13 +0200

    definition of reusable preprocess def

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,12 @@
-import torchvision.transforms as transforms
+import torch
+import numpy as np
+import cv2
 
-transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1), # setting image to grayscale and returning only one channel
-    transforms.CenterCrop(64), # crop the image to center, if input is to small --> padding and then crop
-    transforms.Normalize((0.5),(0.5)) #'''still have to get the means, so far 0.5 as base value'''
-    ])
-
+def preprocess_img(frame, target_size=(64, 64)):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(gray, target_size)
+    gray = gray / 255.0
+    gray = np.expand_dims(gray, axis=-1)
+    gray = np.expand_dims(gray, axis=0)
+    gray_tensor = torch.tensor(gray, dtype=torch.float32).permute(0, 3, 1, 2)
+    return gray_tensor
