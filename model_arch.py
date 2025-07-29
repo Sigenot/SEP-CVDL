40281139fd8188e3cdf909f97d0f928d8b2d5613
Sigenot <tobias.kreil@proton.me>
2025-07-29 23:54:56 +0200

    reworked model architecture for better results

--- model_arch.py
+++ model_arch.py
@@ -5,47 +5,81 @@ class EmotionCNN(nn.Module):
     def __init__(self):
         super(EmotionCNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Dropout(0.25)
-        )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Dropout(0.25)
         )
-        self.layer3 = nn.Sequential(
+        self.layer2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Dropout(0.25)
         )
-        self.layer4 = nn.Sequential(
+        self.layer3 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
+            nn.ReLU(),
+            #nn.MaxPool2d(2),
+            nn.Dropout(0.25)
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            #nn.MaxPool2d(2),
+            nn.Dropout(0.25)
+        )
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+            nn.BatchNorm2d(1024),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Dropout(0.25)
         )
-        self.fc1 = nn.Sequential(
-            nn.Linear(4 * 4 * 256, 512),
+        """
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+            nn.BatchNorm2d(1024),
             nn.ReLU(),
-            nn.Dropout(0.5)
+            #nn.MaxPool2d(2),
+            nn.Dropout(0.25)
         )
-        self.fc2 = nn.Linear(512, 6) # 6 emotions
+        """
+        self.pool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc1 = nn.Linear(1024, 2048)
+        self.fc2 = nn.Linear(2048, 1024)
+
+        self.dropout1 = nn.Dropout(0.2)
+        self.dropout2 = nn.Dropout(0.5)
+
+        self.fc3 = nn.Linear(1024, 6) # 6 emotions
     
     def forward(self, x):
         out = self.layer1(x)
+        #print(out.shape)
         out = self.layer2(out)
+        #print(out.shape)
         out = self.layer3(out)
+        #print(out.shape)
         out = self.layer4(out)
         #print(out.shape)
+        out = self.layer5(out)
+        #print(out.shape)
+        #out = self.layer6(out)
+        #print(out.shape)
+        #out = self.layer7(out)
+        #print(out.shape)
+        #print(out.shape)
+        out = self.pool(out)
+        #print(out.shape)
         out = out.reshape(out.size(0), -1) #flatten tensors
+        #print(out.shape)
         out = self.fc1(out)
         out = self.fc2(out)
+        out = self.dropout1(out)
+        out = self.dropout2(out)
+        out = self.fc3(out)
         return out
